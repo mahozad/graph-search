ca1b77d3c9f322166d0c3e4ffbd72a2476826044
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-05-18 22:53:18 +0430

    Add time measurement for program

--- src/main/kotlin/ir/ac/yazd/Main.kt
+++ src/main/kotlin/ir/ac/yazd/Main.kt
@@ -2,12 +2,15 @@ package ir.ac.yazd
 
 import com.github.junrar.Archive
 import java.io.File
+import java.time.Duration
+import java.time.Instant
 import javax.xml.parsers.SAXParserFactory
 
 fun main() {
     val sourceFiles = arrayOf(File("data/WIR-Part1.rar"), File("data/WIR-Part2.rar"))
     val parser = SAXParserFactory.newInstance().newSAXParser()
     val handler = ParseHandler()
+    val startTime = Instant.now()
 
     for (sourceFile in sourceFiles) {
         val archive = Archive(sourceFile, null)
@@ -17,4 +20,6 @@ fun main() {
         }
     }
     handler.close()
+
+    println("Time: ${Duration.between(startTime, Instant.now()).toMinutes()}m")
 }

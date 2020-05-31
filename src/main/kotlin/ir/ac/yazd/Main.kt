8544cdd72a23fbde4f050aba2a59a93f84f541ab
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-06-01 01:43:11 +0430

    Add initial impl. of searching queries

--- src/main/kotlin/ir/ac/yazd/Main.kt
+++ src/main/kotlin/ir/ac/yazd/Main.kt
@@ -3,8 +3,8 @@ package ir.ac.yazd
 import com.github.junrar.Archive
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.index.DirectoryReader
-import org.apache.lucene.queryparser.classic.MultiFieldQueryParser
-import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.*
+import org.apache.lucene.search.BooleanClause.Occur
 import org.apache.lucene.search.similarities.BM25Similarity
 import org.apache.lucene.store.Directory
 import org.apache.lucene.store.MMapDirectory
@@ -13,9 +13,6 @@ import org.xml.sax.helpers.DefaultHandler
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.attribute.BasicFileAttributes
-import java.time.Duration
-import java.time.Instant
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit.DAYS
 import java.util.function.BiPredicate
@@ -66,56 +63,68 @@ fun query() {
             val parser = SAXParserFactory.newInstance().newSAXParser()
             val handler = object : DefaultHandler() {
                 var currentElement = ""
-                var currentDocid=0
-                val docs = mutableMapOf<Int,Boolean>()
+                var currentDocId = 0
                 val terms = mutableListOf<String>()
+                val labels = mutableMapOf<Int, Boolean>()
 
-                override fun startElement(uri: String?, localName: String?, qName: String, attributes: Attributes?) {
-                    currentElement = qName
+                override fun startElement(uri: String, localName: String, name: String, attrs: Attributes) {
+                    currentElement = name
                 }
 
                 override fun characters(ch: CharArray, start: Int, length: Int) {
-                    if (ch.concatToString(start, start + length)=="\n") return
+                    if (ch.concatToString(start, start + length) == "\n") return
                     else if (currentElement == "word") terms.add(ch.concatToString(start, start + length))
-                    else if (currentElement=="docid") currentDocid = ch.concatToString(start, start + length).toInt()
-                    else if (currentElement == "label") docs[currentDocid] = ch.concatToString(start,start+length)=="1"
+                    else if (currentElement == "docid") currentDocId = ch.concatToString(start, start + length).toInt()
+                    else if (currentElement == "label") labels[currentDocId] = ch.concatToString(start, start + length) == "1"
                 }
 
                 override fun endDocument() {
-                    println(terms)
-                    // search(terms)
+                    search(terms, labels)
                 }
             }
             parser.parse(Files.newInputStream(it), handler)
         }
 }
 
-fun search() {
-    val directory: Directory = MMapDirectory(Path.of("E:/index"))
+fun search(terms: List<String>, docs: Map<Int, Boolean>) {
+    val directory: Directory = MMapDirectory(Path.of("E:/index-standard/"))
     val reader = DirectoryReader.open(directory)
     val searcher = IndexSearcher(reader)
     searcher.similarity = BM25Similarity() // Use BM25 algorithm instead of TF.IDF for scoring documents
     val analyzer = StandardAnalyzer()
 
-    val input = "زنبور عسل"
-
-    // OR TermsQuery class
-    val query = MultiFieldQueryParser(arrayOf("TITLE", "BODY"), analyzer).parse(input)
+    // TermsQuery class
     // OR
-    // val titleQuery = PhraseQuery(5, "TITLE", input)
-    // val bodyQuery = PhraseQuery(5, "BODY", input)
-    // val query: Query = BooleanQuery.Builder()
-    //     .add(titleQuery, BooleanClause.Occur.SHOULD)
-    //     .add(bodyQuery, BooleanClause.Occur.MUST)
+    // val query = MultiFieldQueryParser(arrayOf("TITLE", "BODY"), analyzer).parse(input)
+    // OR
+    // val query = BooleanQuery.Builder()
+    //     .add(TermInSetQuery("TITLE", terms.map { BytesRef(it) }), Occur.SHOULD)
+    //     .add(TermInSetQuery("BODY", terms.map { BytesRef(it) }), Occur.SHOULD)
     //     .build()
     // OR
     // val parser = QueryParser("TITLE", analyzer)
     // val query = parser.parse(input)
-    val hits = searcher.search(query, 10).scoreDocs
+    // OR
+    val titleQuery = PhraseQuery(10, "TITLE", *terms.toTypedArray())
+    val bodyQuery = PhraseQuery(10, "BODY", *terms.toTypedArray())
+    val query: Query = BooleanQuery.Builder()
+        .add(titleQuery, Occur.SHOULD)
+        .add(bodyQuery, Occur.SHOULD)
+        .build()
+    val hits = searcher.search(query, 20).scoreDocs
 
-    // Iterate through the results:
-    for (hit in hits) println(searcher.doc(hit.doc))
+    val precision = hits
+        .filter { docs.containsKey(getDocId(searcher, it.doc)) }
+        .map { if (docs.getValue(getDocId(searcher, it.doc))) 1.0 else 0.0 }
+        .fold(0.0) { acc, b -> acc + b }
+        .div(hits.size)
+
+    println("${precision * 100}%")
 
     reader.close()
     directory.close()
+}
+
+fun getDocId(searcher: IndexSearcher, docNumber: Int): Int {
+    return searcher.doc(docNumber).getField("DOCID").numericValue().toInt()
 }

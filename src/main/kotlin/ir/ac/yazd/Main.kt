92fcd6749e8dc6970df18f76a40ddb29dca27d98
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-05-31 16:46:49 +0430

    Implement reading queries

--- src/main/kotlin/ir/ac/yazd/Main.kt
+++ src/main/kotlin/ir/ac/yazd/Main.kt
@@ -8,12 +8,17 @@ import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.search.similarities.BM25Similarity
 import org.apache.lucene.store.Directory
 import org.apache.lucene.store.MMapDirectory
+import org.xml.sax.Attributes
+import org.xml.sax.helpers.DefaultHandler
 import java.io.File
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
 import java.time.Duration
 import java.time.Instant
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit.DAYS
+import java.util.function.BiPredicate
 import javax.xml.parsers.SAXParserFactory
 
 @ExperimentalStdlibApi
@@ -23,7 +28,7 @@ fun main() {
     // val duration = Duration.between(startTime, Instant.now())
     // println("Time: ${duration.toMinutes()}m")
 
-    search()
+    query()
 }
 
 @ExperimentalStdlibApi
@@ -53,6 +58,38 @@ fun index() {
     indexer.close() // Required
 }
 
+@ExperimentalStdlibApi
+fun query() {
+    val path = Path.of("data/queries")
+    Files.find(path, 1, BiPredicate { f, _ -> f.fileName.toString().startsWith("query") })
+        .forEach {
+            val parser = SAXParserFactory.newInstance().newSAXParser()
+            val handler = object : DefaultHandler() {
+                var currentElement = ""
+                var currentDocid=0
+                val docs = mutableMapOf<Int,Boolean>()
+                val terms = mutableListOf<String>()
+
+                override fun startElement(uri: String?, localName: String?, qName: String, attributes: Attributes?) {
+                    currentElement = qName
+                }
+
+                override fun characters(ch: CharArray, start: Int, length: Int) {
+                    if (ch.concatToString(start, start + length)=="\n") return
+                    else if (currentElement == "word") terms.add(ch.concatToString(start, start + length))
+                    else if (currentElement=="docid") currentDocid = ch.concatToString(start, start + length).toInt()
+                    else if (currentElement == "label") docs[currentDocid] = ch.concatToString(start,start+length)=="1"
+                }
+
+                override fun endDocument() {
+                    println(terms)
+                    // search(terms)
+                }
+            }
+            parser.parse(Files.newInputStream(it), handler)
+        }
+}
+
 fun search() {
     val directory: Directory = MMapDirectory(Path.of("E:/index"))
     val reader = DirectoryReader.open(directory)
@@ -62,6 +99,7 @@ fun search() {
 
     val input = "زنبور عسل"
 
+    // OR TermsQuery class
     val query = MultiFieldQueryParser(arrayOf("TITLE", "BODY"), analyzer).parse(input)
     // OR
     // val titleQuery = PhraseQuery(5, "TITLE", input)
